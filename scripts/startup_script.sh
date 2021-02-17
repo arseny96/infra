fe647aa8202e7b36d3530a760e6bd64f0f3bc6fe
Arseny Shybin <arseny.sh@icloud.com>
2021-02-17 15:07:22 +0300

    few comments

--- scripts/startup_script.sh
+++ scripts/startup_script.sh
@@ -1,20 +1,17 @@
 #!/bin/bash
 
-# install ruby and dependencies
 wget -O - https://raw.githubusercontent.com/Otus-DevOps-2019-08/whoami-io_infra/cloud-testapp/install_ruby.sh | bash
 if [ $? -ne 0 ]; then
   echo "Failed to install ruby"
   exit 1
 fi
 
-# install db
 wget -O - https://raw.githubusercontent.com/Otus-DevOps-2019-08/whoami-io_infra/cloud-testapp/install_mongodb.sh | bash
 if [ $? -ne 0 ]; then
   echo "Failed to install mongo"
   exit 1
 fi
 
-# run app
 cd /home/appuser
 
 wget -O - https://raw.githubusercontent.com/arseny96/infra/base-on-packer/scripts/deploy.sh | sudo bash
@@ -22,4 +19,9 @@ if [ $? -ne 0 ]; then
   echo "Failed to deploy reddit"
   exit 1
 fi
+
+
+
+
+
 
