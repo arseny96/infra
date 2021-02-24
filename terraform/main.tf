e7511e25ce595fb6a0112951161245afbee7a70c
Arseny Shybin <arseny.sh@icloud.com>
2021-02-24 21:46:11 +0300

    adding a few variables

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,55 +1,58 @@
 provider "google" {
-	project = var.project
-	region = var.region
-	zone = "europe-west1-b"
+  project = var.project
+  region  = var.region
+  zone    = var.zone
 }
 
 resource "google_compute_instance" "app" {
-	name = "reddit-app"
-	machine_type = "e2-micro"
-	zone = "europe-west1-b"
-	tags = ["reddit-app"]
-	boot_disk {
-		initialize_params {
-			image = var.disk_image
-		}
-	}
+  name         = "reddit-app"
+  machine_type = "e2-micro"
+  zone         = "europe-west1-b"
+  tags         = ["reddit-app"]
+  boot_disk {
+    initialize_params {
+      image = var.disk_image
+    }
+  }
+  metadata = {
+	ssh-keys = "appuser:${file(var.public_key_path)}"
+  }  
+  network_interface {
+    network = "default"
+    access_config {}
+  }
 
-	metadata = {
-		ssh-keys = "appuser:${file(var.public_key_path)}"
-	}
+  connection {
+    type        = "ssh"
+    user        = "appuser"
+    host        = google_compute_instance.app.network_interface.0.access_config.0.nat_ip
+    agent       = false
+    private_key = file(var.private_key_path)
+  }
 
-	network_interface {
-		network = "default"
-		access_config {}
-	}
+  provisioner "file" {
+    source      = "files/puma.service"
+    destination = "/tmp/puma.service"
+  }
 
-	connection {
-		type = "ssh"
-		user = "appuser"
-		host = google_compute_instance.app.network_interface.0.access_config.0.nat_ip
-		agent = false
-		private_key = file("~/.ssh/appuser")
-	}
-
-	provisioner "file" {
-		source = "files/puma.service"
-		destination = "/tmp/puma.service"
-	}
-
-	provisioner "remote-exec" {
-		script = "files/deploy.sh"
-	}
+  provisioner "remote-exec" {
+    script = "files/deploy.sh"
+  }
 
 }
 
 resource "google_compute_firewall" "firewall_puma" {
-        name = "allow-puma-default"
-        network = "default"
-        allow {
-                protocol = "tcp"
-                ports = ["9292"]
-        }
-        source_ranges = ["0.0.0.0/0"]
-        target_tags = ["reddit-app"]
+  name    = "allow-puma-default"
+  network = "default"
+  allow {
+    protocol = "tcp"
+    ports    = ["9292"]
+  }
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["reddit-app"]
+}
+
+resource "google_compute_project_metadata_item" "metadata" {
+	key = "ssh-keys"
+	value = "vbn:ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDXVx/Xuii7c/MkJaNJnCQXbSufH6GS2m05XYxIErWAZ0D2Q+2xOddOaW9LLEtTsnBvbfjI1CsULLgCrUWvormdmi1c8hepS2ZWtCaGti1hIEwRPo0EeBnuHRqxQ21dgLSICu/xj102tVVt7/dPQt/DSSLcYMKgNUsLvKow0CNQIpyUsOOIEivINSJGn5qDSsxYUEhYsr6mYgNAYIOWjb22CgE/wkRkcolKOZtK60D6COV+3gIkMZWAaoUG5+EBkfU7ZAWYIf7RZxTlpZDXgOcpnTvCm3zUN3GOiUVVWy8VkO3wdTelBYrQLZpOqS+jqPuegEV7l5ZlTDKFiZX03MsrdXgkAculFtF9h2KVJoHkYje2IJkltTrmgpOr9PQgGFlOr8sw9XaWyXDxW75KBjAtkXNu5/hLVH7iyFCnNw/+Xl/upd0cEExIRziGnE2ICmZ218zxgrfNjG2cI6Y2dF7SwpmHTXj47ubPlHOpSyw6fMXJ/wJM0dMm1Yb6fNCMD2c= vbn"
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,16 +1,27 @@
-variable project {
-	description = "Project ID"
+variable "project" {
+  description = "Project ID"
 }
 
-variable region {
-	description = "Region"
-	default = "europe-west1"
+variable "region" {
+  description = "Region"
+  default     = "europe-west1"
 }
 
-variable public_key_path {
-	description = "Path to the public key used for ssh access"
+variable "zone" {
+  description = "Zone"
+  default     = "europe-west1-b"
 }
 
-variable disk_image {
-	description = "Disk image"
+variable "public_key_path" {
+  description = "Path to the public key used for ssh access"
 }
+
+variable "private_key_path" {
+  description = "Path to the private key used for ssh access"
+}
+
+
+variable "disk_image" {
+  description = "Disk image"
+}
+
